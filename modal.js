ebe69f7c80f362542b95492219d842cc811dc552
Melyna Kim <75linaa@gmail.com>
2022-07-31 18:56:00 +0200

    MAJ FORMULAIRE OK; OPENMODALE

--- modal.js
+++ modal.js
@@ -63,32 +63,25 @@ const formDataLocations = document.querySelector("#locations-container"); // inp
 function validateForm(event){
   event.preventDefault();
   let valid = true;
-  function resetFormAttr() {
-    for (const locationForm of form) {
-      const getLocationFormAttr = formDataLocations.getAttribute("data-error-visible");
-      if (getElementAttr){
-        formDataLocations.setAttribute("data-error-visible", "false");
-      }
-    }
-  }
+
   // CONSTANTE POUR FIRST NAME (PRÉNOM)
   const form = event.currentTarget;
   const firstName = form.first.value;
   if (firstName === "" || firstName.length < 2) {
     console.error("Chaîne de caractère trop petite");
     formDataFirst.parentNode.setAttribute("data-error-visible", "true");
-    formDataFirst.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite")
+    formDataFirst.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite");
     valid = false;
   } else {
     formDataFirst.parentNode.setAttribute("data-error-visible", "false");
-    formDataFirst.parentNode.setAttribute("data-error-display", "false");
   }
+
   // CONSTANTE POUR LAST NAME (NOM)
   const lastName = form.last.value;
   if (lastName === "" || lastName.length < 2) {
     console.error("Chaîne de caractère trop petite");
     formDataLast.parentNode.setAttribute("data-error-visible", "true");
-    formDataLast.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite")
+    formDataLast.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite");
     valid = false;
   } else {
     formDataLast.parentNode.setAttribute("data-error-visible", "false");
@@ -110,6 +103,7 @@ function validateForm(event){
   } else {
     formDataEmail.parentNode.setAttribute("data-error-visible", "false");
   }
+
   // CONSTANTE POUR BIRTHDATE (DATE DE NAISSANCE)
   //! ⚠️ POUR UNE DATE MINIMUM (EX : AVANT LA MAJORITÉ) FAUT-IL FAIRE UN NEW DATE ?
   const birthdateForm = form.birthdate.value;
@@ -121,12 +115,13 @@ function validateForm(event){
   } else {
     formDataBirthdate.parentNode.setAttribute("data-error-visible", "false");
   }
+
   // CONSTANTE POUR QUANTITY (NOMBRE TOURNOIS)
     const quantityForm = form.quantity.value;
     if (quantityForm === "" || quantityForm.length < 1) {
       console.error("Chaîne de caractère trop petite");
       formDataQuantity.parentNode.setAttribute("data-error-visible", "true");
-      formDataQuantity.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite")
+      formDataQuantity.parentNode.setAttribute("data-error", "Chaîne de caractère trop petite");
       valid = false;
     } else {
       formDataQuantity.parentNode.setAttribute("data-error-visible", "false");
@@ -134,10 +129,11 @@ function validateForm(event){
 
   // CONSTANTE POUR QUANTITY (QUEL TOURNOIS)
     const locationForm = form.location;
-    if (!locationForm.checked) {
+    console.log(locationForm.value);
+    if (locationForm.value === "") {
       console.error("Pour finaliser votre inscription, merci de cocher un lieu");
       formDataLocations.setAttribute("data-error-visible", "true");
-      formDataLocations.setAttribute("data-error", "Pour finaliser votre inscription, merci de cocher un lieu")
+      formDataLocations.setAttribute("data-error", "Pour finaliser votre inscription, merci de cocher un lieu");
       valid = false;
     } else {
       formDataLocations.setAttribute("data-error-visible", "false");
@@ -145,7 +141,16 @@ function validateForm(event){
 
     if (valid === true) {
       closeModal();
+      openConfirmationModal();
     }
+}
+
+document.getElementById('modalAnimation').style.display = 'none';
+
+function openConfirmationModal() {
+  document.getElementById('modalAnimation').style.display = 'block';
+  //console.log(confirmationContainer);
+  //confirmationContainer.style.display = "block";
 }
 /*function resetFormAttr(){
   for (const locationForm of formData){
